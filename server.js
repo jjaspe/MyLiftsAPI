1f905ecfbe6a1c624cc569140a2d7b2e1584bf1d
“Juan <Juan.Jaspe@greenwayhealth.com>
2016-07-06 22:16:42 -0400

    Added CORS

--- server.js
+++ server.js
@@ -4,16 +4,30 @@ var bodyParser = require("body-parser");
 var mongodb = require("mongodb");
 var dbConnection = require("./dbConnection");
 
-
 var bodyPartsService = require("./bodyparts");
 var usersService = require("./users");
 var exercisesService = require("./exercises");
 var workoutController = require("./workouts");
 var setsController = require("./sets");
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
 var app = express();
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
+app.use(allowCrossDomain);
 
 if(process)
     var port=process.env.PORT
